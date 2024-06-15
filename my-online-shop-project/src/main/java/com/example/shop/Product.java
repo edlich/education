d77413307621f6bad4ceaf862d9f18708294eb4e
stephanhfmnn <149309047+stephanhfmnn@users.noreply.github.com>
2024-06-15 17:22:02 +0200

    Füge mein Online-Shop-Projekt hinzu

---
+++ my-online-shop-project/src/main/java/com/example/shop/Product.java
@@ -0,0 +1,38 @@
+package com.example.shop;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+}// diff anwenden
